d89c2cc2e932627da65d9e4fa7c3a437981eb24b
oudin-clement <clementoudin3@gmail.com>
2024-01-25 17:17:19 +0100

    page pour creer une activité mais il faudra ocnfigurer l'appel de l'api

---
+++ src/components/CreerActivite.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  data: () => ({
+    nom: '',
+    color: '#FF0000',
+    rules: [
+      value => {
+        if (value) return true
+
+        return 'Le nom de l\'activite est requis'
+      },
+    ],
+  }),
+  methods:{
+    createActivity() {
+      if(this.nom !== ''){
+        this.reset()
+      }
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Creer Activité</h2>
+
+    <v-form @submit.prevent ref="form">
+      <v-text-field
+          v-model="nom"
+          :rules="rules"
+          label="Nom de l'activite"
+      ></v-text-field>
+      <v-color-picker color="grey" v-model="color"></v-color-picker>
+      <v-container class="mt-3 pa-0">
+      <v-btn type="submit" variant="outlined" color="blue" @click="createActivity">Creer</v-btn>
+      </v-container>
+    </v-form>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Activites.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CreerActivite from '../components/CreerActivite.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Activité</h1>
+    <CreerActivite />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
