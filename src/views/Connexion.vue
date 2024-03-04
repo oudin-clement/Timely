ee5d04b09c458317b8ad1186508da31090d98f34
NicoRiri <nicolate.bernardet@gmail.com>
2024-03-04 12:40:05 +0100

    feat: Ajout tailwindcss + stylisation inscription + connexion

--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -17,13 +17,15 @@ export default {
   methods: {
     ...mapActions(useAuthentificationStore, ["fetchProfile"]),
 
-    connexion(){
-      axios.get("https://timely.edu.netlor.fr/api/profile", {headers: {
+    connexion() {
+      axios.get("https://timely.edu.netlor.fr/api/profile", {
+        headers: {
           'Content-Type': "application/json",
-          'Authorization' : `key=`+this.key,
-        }})
+          'Authorization': `key=` + this.key,
+        }
+      })
           .then(res => {
-            this.fetchProfile({key : this.key, id :res.data.id, name : res.data.name, email : res.data.email})
+            this.fetchProfile({key: this.key, id: res.data.id, name: res.data.name, email: res.data.email})
             this.$router.push("/")
           })
           .catch(e => {
@@ -39,20 +41,16 @@ export default {
 </script>
 
 <template>
-  {{this.name}}
-  <v-container>
-    <v-row justify="center" align="center" class="h-screen">
-      <v-col cols="3">
-        <h1>Connexion</h1>
-        <v-text-field v-model="key" label="clé"/>
-        <v-btn @click="connexion" color="blue" variant="outlined">Connexion</v-btn>
-        <v-alert class="mt-10" v-model="displayErreur" transition="fade-transition" border="start" variant="tonal"
-                 closable color="red-accent-4" title="erreur">
-          <div :key="e" v-for="e in erreur">{{ e }}</div>
-        </v-alert>
-      </v-col>
-    </v-row>
+  <div class="flex flex-col min-h-screen px-[30vw] justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
+      <h1 class="font-bold text-center text-[59px] mb-12">Connexion</h1>
+      <v-text-field v-model="key" label="clé" class="w-[15vw]"/>
+      <v-btn @click="connexion" color="blue" variant="outlined">Connexion</v-btn>
 
-
-  </v-container>
+      <v-alert class="mt-10" v-model="displayErreur" transition="fade-transition" border="start" variant="tonal"
+               closable color="red-accent-4" title="erreur">
+        <div :key="e" v-for="e in erreur">{{ e }}</div>
+      </v-alert>
+    </div>
+  </div>
 </template>
--- src/views/Inscription.vue
+++ src/views/Inscription.vue
@@ -29,20 +29,20 @@ export default {
 </script>
 
 <template>
-  <v-container>
-    <v-row justify="center" align="center" class="h-screen">
-      <v-col cols="3">
-        <h1>Inscription</h1>
-        <v-text-field v-model="nom" label="nom"/>
-        <v-text-field v-model="email" label="email"/>
+  <div class="flex flex-col min-h-screen px-[30vw] justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
+      <h1 class="font-bold text-center text-[59px] mb-12">Inscription</h1>
+      <v-text-field v-model="nom" label="nom" class="w-[15vw]"/>
+      <v-text-field v-model="email" label="email" class="w-[15vw]"/>
+      <div class="flex justify-center">
         <v-btn @click="inscription" color="blue" variant="outlined">Inscription</v-btn>
-        <v-alert class="mt-10" v-model="displayErreur" transition="fade-transition" border="start" variant="tonal"
-                 closable color="red-accent-4" title="erreur">
-          <div :key="e" v-for="e in erreur">{{ e }}</div>
-        </v-alert>
-      </v-col>
-    </v-row>
+      </div>
+      <v-alert class="mt-10" v-model="displayErreur" transition="fade-transition" border="start" variant="tonal"
+               closable color="red-accent-4" title="erreur">
+        <div :key="e" v-for="e in erreur">{{ e }}</div>
+      </v-alert>
+    </div>
 
 
-  </v-container>
+  </div>
 </template>
