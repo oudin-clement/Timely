ba934a7f6b9afd5bd1a8f37b555ac409ca58dbad
oudin-clement <clementoudin3@gmail.com>
2024-03-08 21:45:33 +0100

    merge du routeur

--- src/router/index.js
+++ src/router/index.js
@@ -90,80 +90,14 @@ const router = createRouter({
                 title: "Paramètre généraux",
             },
             component: () => import('../views/PrametresGeneraux.vue')
+        },
+        {
+            path: '/statistiques',
+            name: 'statistiques',
+            component: () => import('../views/Statistiques.vue')
         }
     ]
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/inscription',
-      name: 'inscription',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Inscription.vue')
-    },
-    {
-      path: '/connexion',
-      name: 'connexion',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Connexion.vue')
-    },
-    {
-      path: '/activite/creer',
-      name: 'creer-activite',
-      component: () => import('../components/CreerActivite.vue')
-    },
-    {
-      path: '/activite',
-        name: 'activite',
-        component: () => import('../views/Activites.vue')
-    },
-    {
-        path: '/profil',
-        name: 'profil',
-        component: () => import('../views/Profil.vue')
-    },
-    {
-        path: '/modif/activite/:id',
-        name: 'modifierActivite',
-        component: () => import('../components/ModiferActivite.vue')
-    },
-    {
-      path: '/modif/projet/:id',
-      name: 'modifierProjet',
-      component: () => import('../components/ModiferProjet.vue')
-    },
-    {
-      path: '/projet',
-      name: 'projet',
-      component: () => import('../views/Projects.vue')
-    },
-    {
-      path: '/parametre/generaux',
-      name: 'parametre-generaux',
-      component: () => import('../views/PrametresGeneraux.vue')
-    },
-    {
-      path: '/statistiques',
-        name: 'statistiques',
-        component: () => import('../views/Statistiques.vue')
-    }
-  ]
+
 })
 
 router.afterEach((to, from, failure) => {
